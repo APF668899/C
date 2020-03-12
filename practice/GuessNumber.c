a8c52c2371406708a10ce88916d195cbaecb40eb
APF668899 <59566818+APF668899@users.noreply.github.com>
2020-03-12 01:23:43 +0000

    猜数

--- practice/GuessNumber.c
+++ practice/GuessNumber.c
@@ -1,37 +1,26 @@
-#include <stdio.h>/ /猜数
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-int main( )
+int main()
 {
-srand(time(0));
-
-int number=rand()%100+1;//生成一个100以内的数
-
-int n=0;
-
-int a=0;
-
-printf( "我以经想好了1到100之间的数\n");while(a!=number)
-
-printf( "请猜这个1到100之间的数: \n");scanf( "%d", &a);
-
-n++ ;
-
-if (a > number )
-
-pr intf( "你猜的数大了\n");
-
-} else if (a < number)
-
-{
-
-printf( "你猜的数小了\n");
-
-}
-
-}
-
-printf("太好了,你用了%d次猜对了答案\n"，n);return 0 ;
-
+	srand(time(0));
+	int number = rand()%1000 + 1;
+	int n = 0;
+	int a = 0;
+	printf("我以经想好了一个数\n");
+	while (a != number)
+	{
+		printf("请猜这个1到1000之间的数:\n");
+		scanf("%d",&a);
+		n++;
+		if (a > number)
+		{
+			printf("你猜的数大了\n");
+		} else if(a < number)
+		{
+			  printf("你猜的数小了\n");
+		}
+	}
+	printf("太好了，你用了%d次猜对了答案\n",n);
+	return 0;
 }
